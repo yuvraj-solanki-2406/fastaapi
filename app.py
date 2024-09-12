7232bb1bfcba54c7a1cc96988c07a6198569b825
Yuvraj Singh Solanki <111236237+yuvraj-solanki-2406@users.noreply.github.com>
2024-09-12 14:30:19 +0530

    fastapi deployment

---
+++ app.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return {"page_name": "Home Page"}
+
+@app.post("/login")
+def login(req):
+    name = req.name
+    email = req.email
+    return {"info": [name, email]}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="localhost", port=8000)
